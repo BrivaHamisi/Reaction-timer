dc968c55225d243c7d2486fa76d46d9133315bc1
Nkunja <munene01denis@gmail.com>
2024-08-26 18:10:17 +0300

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
 
+import Block from './components/Block.vue';
+
 export default {
   name: 'App',
   components: {
+    Block,
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay: null,
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random()*5000
+    }
   }
+
 }
 </script>
 
@@ -17,7 +35,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
